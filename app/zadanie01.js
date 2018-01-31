cb1210aa3d7ced0ebfaf925b616a0d1511ce9b4d
Greg Herbowicz <herbowicz@users.noreply.github.com>
2018-01-31 12:27:58 +0100

    ok

--- app/zadanie01.js
+++ app/zadanie01.js
@@ -1,19 +1,13 @@
+const crypto = require('crypto');
 const MY_PWD_HASH = '5dca0fc4e306d92b2077ad85e7c4bd87a3e8648e';
 
-const crypto = require('crypto');
-
-const algorithm = ['aes192', 'aes-256-cbc', 'aes-256-ecb'];
 const password = ['??TegoHasła', 'CodersLab', 'Node.js Szyfruje Pliki', 'Zaźółć Gęślą Jaźń', 'Moje Haslo 1@3!', '111#$((@)n', 'Dzisiaj Szyfruje 83'];
+const algorithm = ['sha256', 'sha512', 'md5', 'rmd160'];
 
 password.forEach(pass => {
   algorithm.forEach(alg => {
-    const decipher = crypto.createDecipher(alg, pass);
-    let decrypted = decipher.update(MY_PWD_HASH, 'hex', 'utf8');
-    try {
-      decrypted += decipher.final('utf8');
-      console.log(decrypted);
-    } catch (err) {
-      console.log('złe hasło');
-    }
+    const hash = crypto.createHmac(alg, pass)
+      .digest('hex');
+    hash === MY_PWD_HASH && console.log('hasło: ', pass, 'algorytm: ', alg);
   });
 });
