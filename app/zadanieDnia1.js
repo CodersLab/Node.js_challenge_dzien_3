d2acf3f4eb78efc0848eee73feea113b2797b292
Filip Godziński <godzinski.filip@gmail.com>
2018-01-31 21:49:00 +0100

    log hash in the comment

--- app/zadanieDnia1.js
+++ app/zadanieDnia1.js
@@ -9,7 +9,7 @@ fs.readFile(file, 'utf8', (err, data) => {
     if (err === null){
         const hash = crypto.createHmac('sha256', data)
             .digest('hex');
-        console.log(hash);
+        console.log(hash); // d5b1c451f5a44e4fcce7af164b6470f4ebd06b16c8b65adda696c6c7f328289b
     } else {
         console.log('Błąd podczas odczytu pliku!', err);
     }
