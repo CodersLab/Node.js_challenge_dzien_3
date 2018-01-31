48bf7fee07bb34abe0898ed688ea24ea58a8357a
Filip Godziński <godzinski.filip@gmail.com>
2018-01-31 21:40:48 +0100

    ZadanieDnia 2 - done

--- app/zadanieDnia2.js
+++ app/zadanieDnia2.js
@@ -1,3 +1,24 @@
-const ENCRYPTED_TEXT = '4f9fa8f98650091c4910f5b597773c0a48278cfb001fe4eb3ff47ada85cbf0ed3dc17016b031e1459e6e4d9b001ab6e102c11e834a98dce9530c9668c47b76ee6f09d075d19a38e48b415e067c6ddcfad0d3526c405a4f4f2fb1e7502f303c40';
-
 //Twój kod
+
+const crypto = require('crypto');
+const ENCRYPTED_TEXT = '4f9fa8f98650091c4910f5b597773c0a48278cfb001fe4eb3ff47ada85cbf0ed3dc17016b031e1459e6e4d9b001ab6e102c11e834a98dce9530c9668c47b76ee6f09d075d19a38e48b415e067c6ddcfad0d3526c405a4f4f2fb1e7502f303c40';
+const algorithm = ['aes192', 'aes-256-cbc', 'aes-256-ecb'];
+const text = 'Pobawmy się jak komputerowy Detektyw';
+let password = '';
+
+text.split(' ').forEach( textElem => {
+    password += textElem[0] + textElem[textElem.length-1];
+});
+
+algorithm.forEach( algorithmElem => {
+    const decipher = crypto.createDecipher(algorithmElem, password);
+    
+    let decrypted = decipher.update(ENCRYPTED_TEXT, 'hex', 'utf8');
+    try {
+        decrypted += decipher.final('utf8');
+        console.log(`Good hacking! Decrypted text: \n ${decrypted}`);
+    } catch(err) {
+        console.log("You are a weak hacker.");
+    }
+});
+
