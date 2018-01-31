ad0c889a7ecf4d17821a76fcb327a5e8522da6a4
Jakub Matwiejew <custom@audiocustom.pl>
2018-01-31 22:46:51 +0100

    Dzień 3

--- app/zadanieDnia1.js
+++ app/zadanieDnia1.js
@@ -1,1 +1,18 @@
 //Twój kod
+const fs = require('fs');
+
+const crypto = require('crypto');
+
+const text = process.argv[2];
+
+fs.readFile(text, 'utf8', (err, data) => {
+    if (err === null){
+        const hash = crypto.createHmac('sha256', data)
+            .digest('hex');
+        console.log(hash);
+    } else {
+        console.log(`Błąd...`, err)
+    }
+});
+
+
--- app/zadanieDnia2.js
+++ app/zadanieDnia2.js
@@ -1,3 +1,21 @@
 const ENCRYPTED_TEXT = '4f9fa8f98650091c4910f5b597773c0a48278cfb001fe4eb3ff47ada85cbf0ed3dc17016b031e1459e6e4d9b001ab6e102c11e834a98dce9530c9668c47b76ee6f09d075d19a38e48b415e067c6ddcfad0d3526c405a4f4f2fb1e7502f303c40';
 
 //Twój kod
+
+const crypto = require('crypto');
+
+function decodeText(encodedText, password, algorithm){
+    const decipher = crypto.createDecipher(algorithm, password);
+
+    let decrypted = decipher.update(encodedText, 'hex', 'utf8');
+    decrypted += decipher.final('utf8');
+    return decrypted;
+}
+
+let password = '';
+const hint = "Pobawmy się jak komputerowy Detektyw"
+hint.split(' ').forEach(hint => {
+    password += hint[0] + hint[hint.length - 1];
+});
+
+console.log(decodeText(ENCRYPTED_TEXT, password, 'aes-256-ecb'));
