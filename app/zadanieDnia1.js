a9271d8fa87e0b301f00316eaaae8e8f90d61712
Filip Godziński <godzinski.filip@gmail.com>
2018-01-31 20:49:21 +0100

    ZadanieDnia 1 - done

--- app/zadanieDnia1.js
+++ app/zadanieDnia1.js
@@ -1,1 +1,16 @@
 //Twój kod
+
+const fs = require('fs');
+const crypto = require('crypto');
+
+const file = process.argv[2];
+
+fs.readFile(file, 'utf8', (err, data) => {
+    if (err === null){
+        const hash = crypto.createHmac('sha256', data)
+            .digest('hex');
+        console.log(hash);
+    } else {
+        console.log('Błąd podczas odczytu pliku!', err);
+    }
+});
