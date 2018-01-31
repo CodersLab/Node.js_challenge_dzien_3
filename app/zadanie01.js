0fece951d2a8335a22ded9fb0de45220b4a627f2
Łukasz Bułka <mckornik@poczta.fm>
2018-01-31 21:49:33 +0000

    Day3

--- app/zadanie01.js
+++ app/zadanie01.js
@@ -1,3 +1,18 @@
 const MY_PWD_HASH = '5dca0fc4e306d92b2077ad85e7c4bd87a3e8648e';
 
-//Twój kod
+const crypto = require('crypto');
+
+const data = {
+    key:  [ '??TegoHasła', 'CodersLab', 'Node.js Szyfruje Pliki', 'Zaźółć Gęślą Jaźń', 'Moje Haslo 1@3!', '111#$((@)n', 'Dzisiaj Szyfruje 83'],
+    algorithm: ['sha256', 'sha512', 'md5', 'rmd160']
+}
+
+    
+data.key.forEach(key => {
+  data.algorithm.forEach( algorithm => {
+   let hash = crypto.createHmac(algorithm, key).digest('hex');
+   if (hash == MY_PWD_HASH){
+       console.log('Uzyty klucz to: ' + key + ', a algorytm to: ' + algorithm + '.');
+   }
+  });
+});
--- app/zadanieDnia1.js
+++ app/zadanieDnia1.js
@@ -1,1 +1,16 @@
 //Twój kod
+
+const crypto = require('crypto');
+const fs = require('fs');
+
+const args = process.argv[2];
+
+fs.readFile(args, 'utf8', (err, data) => {
+   if (err === null) {
+       console.log('Odczytalem plik ' + args + '.');
+       let hash = crypto.createHmac('sha256', data).digest('hex');
+       console.log('Suma kontrolna sha256 dla pliku ' + args + ' to: ' + hash + '.');
+   } else {
+       console.log('Nie udalo sie odczytac pliku. );', err);
+   }
+});
