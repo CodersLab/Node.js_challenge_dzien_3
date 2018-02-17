ceeca53150adcc536334ac5fe4538e22201d6cf7
Damian Pietrzyk <dpietrzyk96@gmail.com>
2018-02-17 02:09:09 +0100

    finished tasks from Day 3

--- app/zadanie01.js
+++ app/zadanie01.js
@@ -1,3 +1,34 @@
 const MY_PWD_HASH = '5dca0fc4e306d92b2077ad85e7c4bd87a3e8648e';
 
 //Twój kod
+const crypto = require('crypto');
+
+const
+	algorithms = ['sha256', 'sha512', 'md5', 'rmd160'],
+	texts = [
+		'??TegoHasła',
+		'CodersLab',
+		'Node.js Szyfruje Pliki',
+		'Zaźółć Gęślą Jaźń',
+		'Moje Haslo 1@3!',
+		'111#$((@)n',
+		'Dzisiaj Szyfruje 83',
+	];
+
+
+checkPass(algorithms, texts);
+
+function createHash(algorithm, text) {
+	return crypto.createHmac(algorithm, text).digest('hex');
+}
+
+function checkPass(algorithms, texts) {
+	texts.forEach(text => {
+		algorithms.forEach(algorithm => {
+			if (MY_PWD_HASH === createHash(algorithm, text)) {
+				console.log(`algorithm: ${algorithm}, text: ${text}`);
+				return;
+			}
+		});	
+	});
+}
--- app/zadanieDnia1.js
+++ app/zadanieDnia1.js
@@ -1,1 +1,21 @@
-//Twój kod
+const EXPECTED_HASH = '4f7ae6569b55cb6275423ca1cdf31475e607da1d5204c110a58fb480c96e6eca';
+
+const fs = require('fs');
+const crypto = require('crypto');
+
+const
+	file = process.argv[2] || '',
+	algorithm = 'sha256';
+
+fs.readFile(file, 'utf8', (err, data) => {
+	if (err === null) {
+		let hash = crypto.createHmac(algorithm, data).digest('hex');
+		console.log(hash);
+		if (EXPECTED_HASH === hash)
+			console.log('Ok');
+		else
+			console.log('Bad hash, but why?');	
+	} else {
+		console.log(`We have problem with open file, error: ${err}`);
+	}
+});
