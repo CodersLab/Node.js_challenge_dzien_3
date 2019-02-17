046a645d36410b0fd1ab618134cab04bcaa36e07
Your Name <you@example.com>
2019-02-17 21:13:18 +0100

    WiP

--- app/zadanie01.js
+++ app/zadanie01.js
@@ -1,3 +1,21 @@
 const MY_PWD_HASH = '5dca0fc4e306d92b2077ad85e7c4bd87a3e8648e';
 
 //Twój kod
+const passes = ['??TegoHasła',
+'CodersLab',
+'Node.js Szyfruje Pliki',
+'Zaźółć Gęślą Jaźń',
+'Moje Haslo 1@3!',
+'111#$((@)n',
+'Dzisiaj Szyfruje 83'
+]
+const algorithmTypes = ['sha256', 'sha512', 'md5', 'rmd160'];
+const crypto = require('crypto');
+
+
+const hash = crypto.createHmac(algorithmTypes[3], passes[4])
+    .digest('hex');
+console.log(MY_PWD_HASH.length)    
+console.log(hash)    
+console.log('hash length: ', hash.length)    
+console.log(hash === MY_PWD_HASH);
--- app/zadanieDnia1.js
+++ app/zadanieDnia1.js
@@ -1,1 +1,32 @@
 //Twój kod
+const fs = require('fs');
+const crypto = require('crypto');
+
+const arr = process.argv[2];
+console.log('argument to: ',arr);
+// var text = '';
+fs.readFile(arr, 'utf8', (err, data) => {
+    if (err === null) {
+        console.log('Poprawnie odczytano plik :', data);
+        // const paresData = JSON.parse(data);
+
+        console.log('type to: ' ,typeof data)
+
+        const text = data;
+
+        // console.log('type is: ', typeof paresData)
+        const hash = crypto.createHmac('sha256', text)
+            .digest('hex');
+        console.log('PASS IS: ', hash);
+
+
+
+
+
+
+   } else {
+        console.log('Błąd podczas odczytu pliku!', err);
+    }
+});
+
+// 
