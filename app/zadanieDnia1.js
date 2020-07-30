483c288825aa0ab8f508fb63b331e78c8283d47a
Yurii Drahomaretskyi <yuradrahomaretsky@gmail.com>
2020-07-30 15:23:09 +0200

    done

--- app/zadanieDnia1.js
+++ app/zadanieDnia1.js
@@ -1,1 +1,20 @@
 //Twój kod
+const fs = require('fs');
+const crypto = require('crypto');
+
+const codeFunc = (linkToFile) => {
+    fs.readFile(linkToFile, 'utf8', (err, data) => {
+        if (err === null) {
+            console.log(data);
+            const hash = crypto.createHmac('sha256', data)
+                .digest('hex');
+            console.log(hash);
+        } else {
+            console.log(err);
+        }
+    })
+}
+
+const linkToSomeText = './data/zadanieDnia1/testFile.txt';
+
+codeFunc(linkToSomeText);
